bc452350d06d590c5dac1dbe04f528e5618f8962
anbehindY <yanminthwin@gmail.com>
2023-05-16 21:04:01 +0630

    Create basic structure of the project

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {
+
+  },
+});
+
+export default store;
