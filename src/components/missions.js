ab00c54835b43b4bb229316f15c5f1f0a34f38ed
mercymugambi <mercymugambi2016@gmail.com>
2023-05-17 13:59:01 +0300

    Create dispatch for the missions in mission.js

--- src/components/missions.js
+++ src/components/missions.js
@@ -1,7 +1,18 @@
-const Missions = () => (
-  <>
-    <h1>Missions</h1>
-  </>
-);
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../redux/missions/MissionsSlice';
+
+const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch[getMissions()];
+  }, []);
+
+  return (
+    <>
+      <h1>Missions</h1>
+    </>
+  );
+};
 
 export default Missions;
