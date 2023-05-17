2a6a850e48d70922239538f5c6046809c9d6d9a1
mercymugambi <mercymugambi2016@gmail.com>
2023-05-17 13:38:59 +0300

    Add export to getMission function

--- src/redux/missions/MissionsSlice.js
+++ src/redux/missions/MissionsSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const getMissions = createAsyncThunk('missions/getMissions', async () => fetch('https://api.spacexdata.com/v3/missions').then((res) => res.json()));
+export const getMissions = createAsyncThunk('missions/getMissions', async () => fetch('https://api.spacexdata.com/v3/missions').then((res) => res.json()));
 
 const MissionsSlice = createSlice({
   name: 'missions',
