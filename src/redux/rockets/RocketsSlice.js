c994577a8ed7d4e761ae3f2e9151e15d189f9587
anbehindY <yanminthwin@gmail.com>
2023-05-17 17:57:18 +0630

    Change names of state and variables

--- src/redux/rockets/RocketsSlice.js
+++ src/redux/rockets/RocketsSlice.js
@@ -1,11 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const ROCKETS_URL = 'https://api.spacexdata.com/v3/rockets';
+const baseUrl = 'https://api.spacexdata.com/v3/rockets';
 
-const getRocketsData = createAsyncThunk('get/rocketsData', async () => {
-  const rockets = await axios.get(ROCKETS_URL);
-  return rockets.data;
+export const getRocketsData = createAsyncThunk('rockets/getRocketsData', async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
 });
 
 const initialState = {
@@ -15,8 +15,9 @@ const initialState = {
 };
 
 const RocketsSlice = createSlice({
-  name: 'Rockets',
+  name: 'rocket',
   initialState,
+  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(getRocketsData.pending, (state) => {
@@ -24,19 +25,21 @@ const RocketsSlice = createSlice({
       })
       .addCase(getRocketsData.fulfilled, (state, { payload }) => {
         state.loading = false;
-        const rocketsData = payload.map(({
-          id, rocket_name: rocketName, description, flickr_images: images,
+        state.rockets = payload.map(({
+          id,
+          rocket_name: rocketName,
+          description,
+          flickr_images: images,
         }) => ({
           id,
           rocketName,
           description,
           images,
         }));
-        state.rockets = rocketsData;
       })
       .addCase(getRocketsData.rejected, (state, { payload }) => {
         state.loading = false;
-        state.error = payload;
+        state.error = payload.message;
       });
   },
 });
