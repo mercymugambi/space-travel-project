9c8f3b1ec0b9e6460db3b5f9ec94163068f911fa
mercymugambi <mercymugambi2016@gmail.com>
2023-05-17 13:37:04 +0300

    Add mission slice for pending state

--- src/redux/missions/MissionsSlice.js
+++ src/redux/missions/MissionsSlice.js
@@ -2,5 +2,17 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const getMissions = createAsyncThunk('missions/getMissions', async () => fetch('https://api.spacexdata.com/v3/missions').then((res) => res.json()));
 
+const MissionsSlice = createSlice({
+  name: 'missions',
+  initialstate: {
+    missions: [],
+    loading: false,
+  },
+  extraReducers: {
+    [getMissions.pending]: (state) => {
+      state.loading = true;
+    },
+  },
+});
 
-export default getMissions;
+export default MissionsSlice.reducer;
