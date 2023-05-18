52c25a3cb51479a18dc3348a7f28a4f30b729df9
mercymugambi <mercymugambi2016@gmail.com>
2023-05-18 13:35:02 +0300

    Update status td with correct naming

--- src/components/missions.js
+++ src/components/missions.js
@@ -36,7 +36,7 @@ const Missions = () => {
             <tr key={mission.mission_id}>
               <td>{mission.mission_name}</td>
               <td>{mission.description}</td>
-              <td>{mission.reserved ? 'Joined Mission' : 'Not a Member'}</td>
+              <td>{mission.reserved ? 'Active Member' : 'Not a Member'}</td>
               <td>
                 <button
                   type="button"
