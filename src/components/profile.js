2d3f8192b45951e69140d4fd717a55905b96b1b4
mercymugambi <mercymugambi2016@gmail.com>
2023-05-18 23:37:07 +0300

    Use filter to display mission on profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,14 +3,18 @@ import { useSelector } from 'react-redux';
 const Profile = () => {
   const rockets = useSelector((state) => state.rocket.rockets);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  const missions = useSelector((state) => state.mission.missions);
+  const reservedMissions = missions.filter((mission) => mission.reserved);
   return (
     <div className="profileContainer">
       <div className="missionsProfile">
         <h2>My Missions</h2>
         <div className="missionsContainer">
-          <div className="mission">Mission 1</div>
-          <div className="mission">Mission 2</div>
-          <div className="mission">Mission 3</div>
+          {
+            reservedMissions.map((reservedMission) => (
+              <div key={reservedMission.id} className="missions">{reservedMission.mission_name}</div>
+            ))
+          }
         </div>
       </div>
       <div className="rocketsProfile">
