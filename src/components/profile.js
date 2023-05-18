b619de36ec9468bf964b5ece957602409d0bdb7f
anbehindY <yanminthwin@gmail.com>
2023-05-18 23:13:48 +0630

    Add reserved rockets to my profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,22 +1,30 @@
-const Profile = () => (
-  <div className="profileContainer">
-    <div className="missionsProfile">
-      <h2>My Missions</h2>
-      <div className="missionsContainer">
-        <div className="mission">Mission 1</div>
-        <div className="mission">Mission 2</div>
-        <div className="mission">Mission 3</div>
+import { useSelector } from 'react-redux';
+
+const Profile = () => {
+  const rockets = useSelector((state) => state.rocket.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  return (
+    <div className="profileContainer">
+      <div className="missionsProfile">
+        <h2>My Missions</h2>
+        <div className="missionsContainer">
+          <div className="mission">Mission 1</div>
+          <div className="mission">Mission 2</div>
+          <div className="mission">Mission 3</div>
+        </div>
+      </div>
+      <div className="rocketsProfile">
+        <h2>My Rockets</h2>
+        <div className="rocketsContainer">
+          {
+            reservedRockets.map((reservedRocket) => (
+              <div key={reservedRocket.id} className="rockets">{reservedRocket.rocketName}</div>
+            ))
+          }
+        </div>
       </div>
     </div>
-    <div className="rocketsProfile">
-      <h2>My Rockets</h2>
-      <div className="rocketsContainer">
-        <div className="rocket">Rocket 1</div>
-        <div className="rocket">Rocket 2</div>
-        <div className="rocket">Rocket 3</div>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Profile;
